68ae068e5f7ca51be49dfb86641cab290e4474da
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-06-27 11:09:57 +0300

    The basics of compose
    Composable functions
    Layouts
    Material Design

--- app/src/main/java/com/poetcodes/composelearning/MainActivity.kt
+++ app/src/main/java/com/poetcodes/composelearning/MainActivity.kt
@@ -3,36 +3,66 @@ package com.poetcodes.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.poetcodes.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.height(16.dp))
+        Text("A day in Shark Fin Cove", style = typography.h6)
+        Text("Davenport, California", style = typography.body2)
+        Text("December 2018", style = typography.body2)
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    NewsStory()
+}
+
+@Composable
+fun Greeting(name : String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeLearningTheme {
-        Greeting("Android")
-    }
+fun PreviewGreeting() {
+    Greeting("Android")
 }
+
